5b5011c82a556d971a1110fbe093ca7969ec0a7e
wafuwafu13 <mariobaske@i.softbank.jp>
2020-12-27 11:42:29 +0900

    feat: first component

---
+++ src/GenerateLettersByDragCanvas.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import p5 from "p5";
+
+const GenerateLettersByDragCanvas = (props) => {
+  window.generateLettersByDragCanvasLetters = props.letters;
+  window.generateLettersByDragCanvasHeight = props.height;
+  window.generateLettersByDragCanvasWidth = props.width;
+  window.generateLettersByDragCanvasFont = props.font;
+  window.generateLettersByDragCanvasFontColor = props.fontColor;
+  window.generateLettersByDragCanvasFontSizeMin = props.fontSizeMin;
+  window.generateLettersByDragCanvasBackgroundColor = props.backgroundColor;
+
+  const generateLettersByDrag = (p) => {
+    let x = 0;
+    let y = 0;
+    let stepSize = 100;
+
+    const font = window.generateLettersByDragCanvasFont || "Georgia";
+    const letters = window.generateLettersByDragCanvasLetters;
+    const fontSizeMin = window.generateLettersByDragCanvasFontSizeMin || 10;
+
+    let counter = 0;
+
+    p.setup = () => {
+      p.createCanvas(
+        window.generateLettersByDragCanvasHeight || 500,
+        window.generateLettersByDragCanvasWidth || 500
+      );
+      p.background(
+        window.generateLettersByDragCanvasBackgroundColor || "#CCCCCC"
+      );
+      p.cursor(p.HAND);
+
+      x = p.mouseX;
+      y = p.mouseY;
+
+      p.textFont(font);
+      p.textAlign(p.LEFT);
+      p.fill(window.generateLettersByDragCanvasFontColor || "#000000");
+    };
+
+    p.draw = () => {
+      const d = p.dist(x, y, p.mouseX, p.mouseY);
+      p.textSize(fontSizeMin + d / 2);
+      const newLetter = letters.charAt(counter);
+
+      stepSize = p.textWidth(newLetter);
+
+      if (d > stepSize) {
+        const angle = p.atan2(p.mouseY - y, p.mouseX - x);
+
+        p.translate(x, y);
+        p.text(newLetter, 0, 0);
+
+        counter++;
+        if (counter >= letters.length) counter = 0;
+
+        x = x + p.cos(angle) * stepSize;
+        y = y + p.sin(angle) * stepSize;
+      }
+    };
+
+    p.mousePressed = () => {
+      x = p.mouseX;
+      y = p.mouseY;
+    };
+
+    p.keyPressed = () => {
+      if (p.key == "s") p.saveCanvas("generateLettersByDrag", "png");
+    };
+  };
+
+  useEffect(() => {
+    new p5(generateLettersByDrag);
+  }, []);
+  return <></>;
+};
+
+export default GenerateLettersByDragCanvas;
